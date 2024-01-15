eedfdb4b0e79e61f312f626c3199fe783e9977ef
Ajay <ajay_kumar_1990@live.com>
2024-01-15 05:44:53 +0530

    skeleton project

---
+++ src/test/java/com/ajaycodes/springbatchexamples/SpringBatchExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajaycodes.springbatchexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
